c541cc448277da1760e5cf710ff55bfe73933807
Magzhan <sunmorring@gmail.com>
2022-11-25 17:07:13 +0600

    first commit

---
+++ src/components/JoinBlock/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './JoinBlock.module.scss';
+
+const JoinBlock = () => {
+  return <div className={styles.wrapp}>index</div>;
+};
+
+export default JoinBlock;
---
+++ src/components/index.ts
@@ -0,0 +1,1 @@
+export { default as JoinBlock } from './JoinBlock/index';
